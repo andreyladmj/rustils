518aa285347c236ac165599db37f8f4798da7b72
Srivoknovskiy <s@>
2024-03-06 09:55:01 +0200

    init

--- pathfinder-ggez/src/main.rs
+++ pathfinder-ggez/src/main.rs
@@ -1,15 +1,24 @@
-use ggez::{Context, ContextBuilder, GameResult, glam};
+mod lib;
+
+use ggez::GameError;
+use ggez::{conf, Context, ContextBuilder, GameResult, glam};
 use ggez::conf::{FullscreenType, WindowMode};
 use ggez::graphics::{self, Color, Image, ImageFormat};
 use ggez::event::{self, EventHandler};
+use ggez::mint::Vector2;
+
+use crate::lib::*;
 
 fn main() {
+
     let (mut ctx, event_loop) = ContextBuilder::new("PathFinder", "Andrii")
+        .backend(conf::Backend::default())
         .build()
         .expect("could not create ggez context!");
 
     let state = MainState::new(&mut ctx);
 
+    ctx.gfx.set_window_title("PathFinder");
     ctx.gfx.set_mode(WindowMode {
         width: 2800.0,
         height: 1700.0,
@@ -31,21 +40,52 @@ fn main() {
 }
 
 struct MainState {
-    worldmap: Image
+    tile1: Image,
+    // tile2: Image,
+    // tile3: Image,
+    // tile4: Image,
+    origin: Point,
+    zoom: f32,
 }
 
 impl MainState {
     pub fn new(_ctx: &mut Context) -> MainState {
-        let worldimage = Image::from_pixels(_ctx, &[150, 150, 150, 150], ImageFormat::R8Unorm, 2, 2);
+        let data = read_bin("assets/bathymetry.bin".to_string());
+        println!("data: {} - {} - {}", data.len(), (21600*10800), 21600.0/8192.0);
+
+        let mut slice1: Vec<u8> = vec![];
+
+        let n_lat = 10800;
+        let n_lon = 21600;
+
+        for idx_lat in 0..8192 {
+            for idx_lon in 0..8192 {
+                // bathymetry[idxLon * this->size.nLat + (this->size.nLat - idxLat - 1)];
+                match data[idx_lon * n_lat + (n_lat - idx_lat - 1)] {
+                    0 => {
+                        slice1.push(150);
+                    },
+                    1 => {
+                        slice1.push(0);
+                    },
+                    _ => {
+                        println!("mismatch: {}", data[idx_lon * n_lat + (n_lat - idx_lat - 1)]);
+                    }
+                }
+            }
+        }
+
+        let tile1 = Image::from_pixels(_ctx, &data, ImageFormat::R8Unorm, 8192, 8192);
         MainState {
-            worldmap: worldimage
+            tile1: tile1,
+            origin: Point::new(0.0, 0.0),
+            zoom: 0.1
         }
     }
 }
 
 impl EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        // Update code here...
         Ok(())
     }
 
@@ -54,9 +94,19 @@ impl EventHandler for MainState {
 
         // canvas.draw(&self.worldmap, graphics::DrawParam::new());
 
-        let dst = glam::Vec2::new(20.0, 20.0);
-        canvas.draw(&self.worldmap, graphics::DrawParam::new().dest(dst));
+        let dst = glam::Vec2::new(0.0, 0.0);
+        canvas.draw(&self.tile1, graphics::DrawParam::new().scale(Vector2 { x: self.zoom, y: self.zoom }).dest(dst));
 
         canvas.finish(ctx)
+    }
+
+    fn mouse_wheel_event(&mut self, _ctx: &mut Context, _x: f32, y: f32) -> Result<(), GameError> {
+        if y > 0.0 {
+            self.zoom = self.zoom - 0.01;
+        } else if y < 0.0 {
+            self.zoom = self.zoom + 0.01;
+        }
+        println!("self.zoom: {}", self.zoom);
+        Ok(())
     }
 }
